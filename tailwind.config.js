2b791f6caa02a8ce68f62bb831c9dfe990bc1cb7
tashenoff <kail.dead.kd@gmail.com>
2023-04-25 13:52:21 -0400

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,34 +7,44 @@ module.exports = {
     "./assets/css/*.scss",
     "./assets/css/*.css"
   ],
- 
+
   theme: {
+
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem',
+        lg: '2rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
       backgroundColor: ["group-hover"],
       divideColor: ['group-hover'],
-        colors: {
-            primary: '#0e1733',
-            darkLight: '#0c162d',
-            primaryLight: '#272f4a',
-            secondary: '#ff033e',
-            secondaryLight: '#ef238a',
-            green: '#47E189',
-            greenDark: '#42cf7e',
-            black: '#333',
-            white: '#fff',
-            red: '#e045e6',
-            gray: '#F4F4F4',
-            blueLight: '#c3d9f6'
-        }
+      colors: {
+        primary: '#0e1733',
+        darkLight: '#0c162d',
+        primaryLight: '#272f4a',
+        secondary: '#ff033e',
+        secondaryLight: '#ef238a',
+        green: '#47E189',
+        greenDark: '#42cf7e',
+        black: '#333',
+        white: '#fff',
+        red: '#e045e6',
+        gray: '#F4F4F4',
+        blueLight: '#c3d9f6'
+      }
     },
 
     zIndex: {
-        '40': '40',
-        '60': '60',
-        '50': '50',
-        '70': '70',
-        '80': '80'
+      '40': '40',
+      '60': '60',
+      '50': '50',
+      '70': '70',
+      '80': '80'
     },
-},
+  },
   plugins: [],
 }
